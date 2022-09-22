021e69a2619dd33e44b49ee3e9a3253769af4eb4
rngustj9139 <mnb9139@khu.ac.kr>
2022-09-22 21:07:38 +0900

    Setting first environment

---
+++ src/main/java/koo/MicroServiceOrderService/MicroServiceOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package koo.MicroServiceOrderService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/koo/MicroServiceOrderService/MicroServiceOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package koo.MicroServiceOrderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
