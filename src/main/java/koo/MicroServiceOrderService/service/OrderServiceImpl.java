fd2a711105299c1a246df3e609613db8569c8997
rngustj9139 <mnb9139@khu.ac.kr>
2022-09-24 11:06:10 +0900

    Add OrderServiceImpl

--- src/main/java/koo/MicroServiceOrderService/service/OrderServiceImpl.java
+++ src/main/java/koo/MicroServiceOrderService/service/OrderServiceImpl.java
@@ -2,24 +2,47 @@ package koo.MicroServiceOrderService.service;
 
 import koo.MicroServiceOrderService.dto.OrderDto;
 import koo.MicroServiceOrderService.jpa.OrderEntity;
+import koo.MicroServiceOrderService.jpa.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.stereotype.Service;
 
-import javax.persistence.criteria.Order;
+import java.util.UUID;
 
+@RequiredArgsConstructor
+@Service
 public class OrderServiceImpl implements OrderService {
 
+    private final OrderRepository orderRepository;
+
     @Override
-    public OrderDto createOrder(OrderDto orderDetails) {
-        return null;
+    public OrderDto createOrder(OrderDto orderDto) {
+        orderDto.setOrderId(UUID.randomUUID().toString());
+        orderDto.setTotalPrice(orderDto.getUnitPrice() * orderDto.getQty());
+
+        ModelMapper mapper = new ModelMapper(); //  ModelMapper는 DTO를 엔티티로 바꿀때 쉽게 바꿀수 있게 해주는 해준다. (pom.xml에서 의존성을 추가해야한다.)
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT); // 딱 맞아 떨어지지 않으면 변환을 못하게 전략 설정
+        OrderEntity orderEntity = mapper.map(orderDto, OrderEntity.class);
+
+        orderRepository.save(orderEntity);
+
+        OrderDto returnValue = mapper.map(orderEntity, OrderDto.class);
+
+        return returnValue;
     }
 
     @Override
     public OrderDto getOrderByOrderId(String orderId) {
-        return null;
+        OrderEntity orderEntity = orderRepository.findByOrderId(orderId);
+        OrderDto orderDto = new ModelMapper().map(orderEntity, OrderDto.class);
+
+        return orderDto;
     }
 
     @Override
     public Iterable<OrderEntity> getOrdersByUserId(String userId) {
-        return null;
+        return orderRepository.findByUserId(userId);
     }
 
 }
